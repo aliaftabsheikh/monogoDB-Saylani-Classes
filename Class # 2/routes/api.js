7710ef75a25517296316f9d5896d3a0abed651ad
Ali Aftab Sheikh <aliaftabsheikh1@gmail.com>
2022-02-16 14:25:29 +0500

    Update data in mongoDB

--- Class # 2/routes/api.js
+++ Class # 2/routes/api.js
@@ -1,26 +1,46 @@
 const express = require("express");
 const router = express.Router();
+const {ObjectId} = require('mongodb')
+const { getConnect } = require("../db")
 
-const posts = [{title : 'My Title', descreption : 'My Description'}];
+const posts = [{ title: 'My Title', descreption: 'My Description' }];
 
-router.get('/posts', (req,res)=>{
+const getAllPosts = async () => {
+    const posts = [];
+    const postsConnect = getConnect().db().collection("posts")
+    const postsCursor = await postsConnect.find();
+    await postsCursor.forEach(post => {
+        posts.push(post)
+    });
+    return posts;
+}
+
+router.get('/posts', async (req, res) => {
+    const posts = await getAllPosts()
     res.status(200).json(posts)
 });
 
-router.post('/post', (req,res)=>{
-    const {title, descreption} = req.body;
-    posts.push({title, descreption})
+router.post('/post', async (req, res) => {
+    const { title, descreption } = req.body;
+    const postsConnect = getConnect().db().collection("posts")
+    await postsConnect.insertOne({ title, descreption });
+    const posts = await getAllPosts()
     res.status(200).json(posts)
 })
- 
-router.patch('/post/:index', (req,res)=>{
-    const index = req.params.index;
-    const {title, descreption} = req.body;
-    posts[index] = {title, descreption}
-    res.status(201).json(posts)
+
+router.patch('/post/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const { title, descreption } = req.body;
+        const postsConnect = getConnect().db().collection("posts")
+        await postsConnect.updateOne({ _id : ObjectId(id)}, { $set: { title, descreption }});
+        res.status(201).json(posts)
+    } catch (e) {
+        res.json(e)
+    }
 })
 
-router.delete('/post/:index', (req,res)=>{
+router.delete('/post/:index', (req, res) => {
     const index = req.params.index;
     posts.splice(index, 1)
     res.status(201).json(posts)
