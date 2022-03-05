fb3c0f244bd068d65d41167270b55792a72be426
Ali Aftab Sheikh <aliaftabsheikh1@gmail.com>
2022-03-05 11:51:15 +0500

    Schema Validation

--- Class # 4/models/product.js
+++ Class # 4/models/product.js
@@ -3,9 +3,26 @@ const { Schema } = mongoose;
 
 const productSchema = new Schema({
     title: { type: String, required: true }, // String is shorthand for {type: String}
-    price: Number,
+    price: {
+        type: Number,
+        validate: {
+            validator : (val)=>{
+                return val > 0;
+            }, 
+            message : 'Price cannot be less then 0'
+        }
+    },
     ratings: Number,
-    inStock: Boolean
+    inStock: Boolean,
+    description : {
+        type: String,
+        validate: {
+            validator : (val)=>{
+                return val.length> 99;
+            }, 
+            message : 'Description cannot be less then 100 Letters'
+        }
+    },
 });
 
 productSchema.static('findByTitle', function (title) {
